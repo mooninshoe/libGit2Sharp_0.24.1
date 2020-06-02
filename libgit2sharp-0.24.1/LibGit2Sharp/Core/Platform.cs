ab3081285631eec8a8662e5cc909bd3f29412b27
Xiang Liu <xiang.liu@advantest.com>
2020-06-03 00:43:00 +0800

    add 0.24.1

---
+++ libgit2sharp-0.24.1/LibGit2Sharp/Core/Platform.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LibGit2Sharp.Core
+{
+    internal enum OperatingSystemType
+    {
+        Windows,
+        Unix,
+        MacOSX
+    }
+
+    internal static class Platform
+    {
+        public static string ProcessorArchitecture => IntPtr.Size == 8 ? "x64" : "x86";
+
+        public static OperatingSystemType OperatingSystem
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return OperatingSystemType.Windows;
+                }
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    return OperatingSystemType.Unix;
+                }
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    return OperatingSystemType.MacOSX;
+                }
+
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
